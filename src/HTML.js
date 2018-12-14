6e07df4c868c6d59648d34bf691e22f996eaa856
Kenny Meyer <kenny@kennymeyer.net>
2018-12-14 17:46:37 -0300

    Disable font scaling by default

--- src/HTML.js
+++ src/HTML.js
@@ -64,7 +64,7 @@ export default class HTML extends PureComponent {
         tagsStyles: {},
         classesStyles: {},
         textSelectable: false,
-        allowFontScaling: true
+        allowFontScaling: false
     }
 
     constructor (props) {
